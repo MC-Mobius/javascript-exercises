5ab998c25e1aa4cfe415363ae6ed2f184a697c82
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-06-02 12:41:59 +0300

    First pass

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArr = string.split("");
+    let revStringArr = [];
 
+    reversedString = ""
+    for (i = 0; i < stringArr.length; i++) {
+        revStringArr.unshift(stringArr[i]);
+    }
+
+    for (i = 0; i < revStringArr.length; i++) {
+        reversedString = reversedString + revStringArr[i];
+    }
+    
+    return reversedString 
 };
 
 // Do not edit below this line
