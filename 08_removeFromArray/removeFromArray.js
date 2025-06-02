655f5ccad35c64d043aa4e63bdd8ea09eeeb3481
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-06-02 12:54:46 +0300

    First pass

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, unwanted) {
+    for (i = 0; i < array.length; i++) {
+        if (i === unwanted) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
