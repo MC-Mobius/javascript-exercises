15d447003671f0658b5ba7effa5998868bb9b586
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-06-02 11:54:07 +0300

    First attempt. Fail due to separate console logs

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    while (i < num) {
+        console.log(string)
+        i ++;
+    } 
 };
 
 // Do not edit below this line
