bf3d15348c33910385c897ed577617816ff00a42
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-06-02 13:00:31 +0300

    Fix if statement comparing unwanted to iteration

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
 const removeFromArray = function(array, unwanted) {
+    console.log(array);
+    console.log(unwanted);
     for (i = 0; i < array.length; i++) {
-        if (i === unwanted) {
+        if (array[i] === unwanted) {
             array.splice(i, 1);
         }
     }
+    console.log(array);
     return array;
 };
 
