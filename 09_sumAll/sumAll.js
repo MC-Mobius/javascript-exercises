5fc54f801c50bd947413f00ec66ba3fdad0fa830
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-06-03 22:28:22 +0300

    First pass

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+
+    let sum = 0
+    let greater
+    let lesser 
+    
+    if (first < second) {
+        lesser = first;
+        greater = second;
+    }
+    else {
+        lesser = second;
+        greater = first;
+    }
+
+    for (i = lesser; i <= greater; i++) {
+        sum = sum + i;
+    }
+
+    return sum
 
 };
 
